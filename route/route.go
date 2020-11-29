173c82def891c7a8d83498631d8a4d502b971302
Schmidt, Tibor <tibor.schmidt@firma.seznam.cz>
2020-11-29 20:07:47 +0100

    wrapping chi router

---
+++ route/route.go
@@ -0,0 +1,22 @@
+package route
+
+import (
+	chi "github.com/go-chi/chi"
+	docgen "github.com/go-chi/docgen"
+	// middleware "github.com/go-chi/chi/middleware"
+)
+
+// Router embeds chi router
+type Router struct {
+	chi.Router
+}
+
+// New returns new instance of router
+func New() Router {
+	return Router{chi.NewRouter()}
+}
+
+// GenerateDocs json docs string for the router
+func GenerateDocs(r Router) string {
+	return docgen.JSONRoutesDoc(r.Router)
+}
