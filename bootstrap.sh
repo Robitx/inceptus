35f0e046db590934033e8a45b1522634bb124b2e
Schmidt, Tibor <tibor.schmidt@firma.seznam.cz>
2021-01-06 14:24:57 +0100

    bootstrap prepares git repo

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,34 +2,27 @@
 
 orig_repository="github.com/robitx/inceptus/_server_template"
 
+delimeter="========================================================================"
+
 usage()
 {
-echo """Hey,
-this script expects you already have existing git repository, into
-which you want to bootstrap server based on inceptus server_template.
+echo """Hello there,
+this script allows you to quickly bootstrap new golang server based on inceptus/_server_template.
+
+Example:
+./bootstrap.sh -n shiny_new_project -r github.com/XXXX/shiny_new_project -d /tmp/shiny_new_project
+
+cd /tmp/shiny_new_project; docker-compose -f docker-compose-dev.yaml up;
 
 Usage:
     bootstrap.sh -h Display this help message.
 
     bootstrap.sh -n PROJECT_NAME -r REPOSITORY -d DIRECTORY
     where:
-    - PROJECT_NAME will be used instead of \"server_template\"
-    - REPOSITORY (like github.com/XXX/YYY) will be used instead of:
-      \"$orig_repository\"
-    - DIRECTORY is a folder where you want to boostrap the server:
-      ├── cmd
-      │   └── server
-      │       └── main.go
-      ├── conf
-      │   ├── PROJECT_NAME.env
-      │   └── PROJECT_NAME.yaml
-      └── internal
-          ├── do
-          │   └── do.go
-          └── env
-              ├── config.go
-              └── environment.go
-      ...
+      - PROJECT_NAME will be used instead of \"server_template\"
+      - REPOSITORY (like github.com/XXX/YYY) will be used instead of:
+        \"$orig_repository\"
+      - DIRECTORY is a folder where you want to boostrap the server
 """
 }
  while getopts "n:r:d:h" opt; do
@@ -59,6 +52,9 @@ then
   usage;
   exit 1;
 fi
+
+echo -e "Trying to make $directory in case it doesn't exists:"
+mkdir -p "$directory";
 
 echo -e "Copying files from _server_template/* to $directory:"
 cp -v -r ./_server_template/* "$directory";
@@ -100,5 +96,23 @@ find "$directory" | grep server_template;
 grep -irl "server_template" $directory;
 echo -e "DONE\n";
 
-echo "Server is ready:"
-tree $directory;
+echo "Prepping git repository:"
+cd $directory;
+git init -b main;
+
+echo -e "Making first commit:"
+git add .;
+git commit -m "bootstraping $name project from inceptus server_template";
+
+echo -e "Setting remote git origin (ssh type):"
+git remote add origin "git@$repository.git"
+git remote -v;
+echo -e "DONE\n";
+
+echo -e "\n\n\n\n$delimeter\nREADME:\n"
+cat README.md
+
+echo -e "\n\n\n\nYour $name project is ready!\n"
+echo "Please read README.md printed above ^"
+echo "(especially the section about HTTPS for local development)"
+
